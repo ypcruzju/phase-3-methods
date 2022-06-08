d6d6aaac2f810695ccc9f80b354c9f16ff067e39
Yax <ypjuarez11@gmail.com>
2022-06-08 10:02:43 -0400

    commit tree e83d5a19f810a9987fff1706ff079d3720edb6a4

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet (name = 'Naureen')
+    puts "Hello, #{name}!"
+
+end
+greet "Jimmy"
+
+def greet_with_default name = 'Naureen'
+    if name == 'Naureen' 
+        puts "Hello, #{name}!"
+    else 
+        puts "Hello, programmer!"
+    end
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve num1
+    #{num1} / 2     
+
+end
